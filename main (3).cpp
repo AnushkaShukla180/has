fd9db05b37dbd05eca6b11e7d732c2531369823b
ANUSHKA SHUKLA <82254904+AnushkaShukla180@users.noreply.github.com>
2022-07-26 00:32:53 +0530

    Add files via upload

---
+++ main (3).cpp
@@ -0,0 +1,25 @@
+/******************************************************************************
+
+                              Online C++ Compiler.
+               Code, Compile, Run and Debug C++ program online.
+Write your code in this editor and press "Run" button to compile and execute it.
+
+*******************************************************************************/
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{  int n1=1,n2=1,n3,i;
+    cout<<n1<<n2;
+    for(i=1;i<=10;i++)
+    
+    n3=n1+n2;
+    n1=n2;
+    n2=n3;
+    {
+    cout<<n3;
+    }   
+    return 0;
+}
